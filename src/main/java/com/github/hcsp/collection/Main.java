320dc05540ba4dc0ab002eb0ce40464e5d93ccef
zaker-666 <1075151259china@gmail.com>
2020-01-06 21:24:54 +0800

    实现了功能

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,33 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        List<User> techDp = new ArrayList<>();
+        List<User> marDp = new ArrayList<>();
+        for (User user : users) {
+            if("技术部".equals(user.getDepartment())){
+                techDp.add(user);
+            }else if("市场部".equals(user.getDepartment())){
+                marDp.add(user);
+            }
+        }
+        techDp.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getAge(),o2.getAge());
+            }
+        });
+        marDp.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getAge(),o2.getAge());
+            }
+        });
+        Map<String, List<User>> resultMap = new HashMap<>();
+        resultMap.put("技术部",techDp);
+        resultMap.put("市场部",marDp);
+        return resultMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
