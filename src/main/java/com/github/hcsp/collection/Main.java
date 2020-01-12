0cb0f35402d87daf68e6fdfdf1a7b9ff09573167
xdc <1434412784@qq.com>
2020-01-12 15:30:05 +0800

    草稿1

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -12,7 +12,28 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        /*
+            做映射和排序
+                1. 映射，字符串加一个用户的 list 的 Map，
+                    其实，题目只有两个部门，一个市场部一个技术部，
+                        只需要提取 user 的名字，分别加入到不同的 Map 中的 list
+                2. 其中每插入一个用户，要做一个年龄的比较（重写比较的方法）,
+              m      不一定，排序等于有序的序列，可以利用 TreeSet ？
+         */
+        Map<String,List<User>> mapUser = new Map
+
+        for (User user :
+                users) {
+            if (user.getDepartment() == "技术部") {
+
+            } else {
+
+            }
+        }
+
+
+    }
 
     public static void main(String[] args) {
         System.out.println(
