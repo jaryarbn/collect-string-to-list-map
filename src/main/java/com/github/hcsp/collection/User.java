076d06a591b7f05245f24d5c4f62b4d9f929fb0a
YongJieLiu <617805327@qq.com>
2020-03-15 23:42:16 +0800

    将List处理成Map<String, List>

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -2,7 +2,7 @@ package com.github.hcsp.collection;
 
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User> {
     // 用户的id
     private final Integer id;
     // 用户的姓名
@@ -37,18 +37,25 @@ public class User {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User person = (User) o;
-        return Objects.equals(id, person.id);
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(department, user.department);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(id, department);
+    }
+
+    @Override
+    public int compareTo(User user) {
+        if (age < user.age) {
+            return -1;
+        } else if (age > user.age) {
+            return 1;
+        }
+        return 0;
     }
 }
