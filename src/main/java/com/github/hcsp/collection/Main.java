1ebc2e20e135f622f666f58a29718ec23b0c316f
summer <summer_xue.zz@foxmail.com>
2020-06-18 18:09:59 +0800

    修改import的包，将java.util.*改成具体的

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,6 +1,13 @@
 package com.github.hcsp.collection;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Arrays;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
