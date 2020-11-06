486719a3d48510f7a690dba1320b1fd6eee38b44
Gordon <sgz886@gmail.com>
2020-11-06 20:25:57 +0800

    采用HashMap<String, List<User>>存储结果 User类增加toString和CompareTo方法

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,33 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        SorUserByAge(users);
+        Map<String, List<User>> departmentMap = new HashMap<>();
+        for (User user :
+                users) {
+            if (!departmentMap.containsKey(user.getDepartment())) {
+                departmentMap.put(user.getDepartment(), new ArrayList<>(Arrays.asList(user)));
+            } else {
+                departmentMap.get(user.getDepartment()).add(user);
+            }
+        }
+//        别人的排序方法,看不懂
+//        for (List<User> list : departmentMap.values()) {
+//            list.sort(Comparator.comparingInt(User::getAge));
+//        }
+        return departmentMap;
+//        for (Map.Entry<String, List<User>> entry :
+//                departmentMap.entrySet()) {
+//            System.out.print("entry.getKey() = " + entry.getKey()+" ; ");
+//            System.out.println("entry.getValue() = " + entry.getValue());
+//        }
+    }
+
+    private static void SorUserByAge(List<User> users) {
+        users.sort(User::compareTo);
+    }
+
 
     public static void main(String[] args) {
         System.out.println(
