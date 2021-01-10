3debbe2c0e803fd1e2d3c669a44dfd955b00b47e
jmq <q1164006765@163.com>
2021-01-10 17:48:29 +0800

    mission complete

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,25 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        sortUserByAge(users);
+        Map<String, List<User>> resultMap = new HashMap<>();
+        users.forEach(user -> {
+            String departmentName = user.getDepartment();
+            if (resultMap.containsKey(departmentName)) {
+                resultMap.get(departmentName).add(user);
+            } else {
+                List<User> list = new ArrayList<>();
+                list.add(user);
+                resultMap.put(departmentName, list);
+            }
+        });
+        return resultMap;
+    }
+
+    private static void sortUserByAge(List<User> users) {
+        users.sort(User::compareTo);
+    }
 
     public static void main(String[] args) {
         System.out.println(
